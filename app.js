0d55ae6438feecfed24fd78224209fc0f88cf86e
Phil <happy.melodist@gmail.com>
2021-12-05 19:29:40 -0600

    select all needed HTML elements into variables

--- app.js
+++ app.js
@@ -251,3 +251,36 @@
 /*
 my code below
 */
+
+const balanceEl = document.querySelector(".balance .value");
+const incomeTotalEl = document.querySelector(".income-total");
+const outcomeTotalEl = document.querySelector(".expense-total");
+const incomeEl = document.querySelector("#income-tracker");
+const expenseEl = document.querySelector("#expense-tracker");
+const allEl = document.querySelector("#all");
+const incomeList = document.querySelector("#income-tracker .list");
+const expenseList = document.querySelector("#expense-tracker .list");
+const allList = document.querySelector("#all .list");
+const lists = document.querySelector(".list");
+
+// all the TABS
+
+const expenseBtn = document.querySelector(".tab1");
+const incomeBtn = document.querySelector(".tab2");
+const allBtn = document.querySelector(".tab3");
+
+// input BUTTONS
+
+const addIncome = document.querySelector(".add-income");
+const incomeTitle = document.querySelector("#income-title-input");
+const incomeAmount = document.querySelector("#income-amount-input");
+
+const addExpense = document.querySelector(".add-expense");
+const expenseTitle = document.querySelector("#expense-title-input");
+const expenseAmount = document.querySelector("#expense-amount-input");
+
+// our NECESSARY VARIABLES
+
+let ENTRY_LIST = [];
+let [balance, income, outcome] = [0, 0, 0];
+let [deleteIcon, editIcon] = ["fas fa-trash", "far fa-edit"];
